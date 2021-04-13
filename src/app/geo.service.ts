9d5f6697375ec2cf631f821a4a71e510fe2a6741
FazioNico <contact@nicolasfazio.ch>
2021-04-13 10:02:46 +0200

    upd: configGeoServie

--- src/app/geo.service.ts
+++ src/app/geo.service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
+import { Geolocation } from '@capacitor/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GeoService {
 
+  async getPosition(): Promise<any> {
+    const position = await Geolocation.getCurrentPosition();
+    return {
+      lat: position.coords.latitude,
+      long: position.coords.longitude
+    };
+  }
 }
